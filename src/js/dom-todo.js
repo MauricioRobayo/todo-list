3140674345257566f28869816220984afd55cd9c
oscarnava <oscarnava@gmail.com>
2019-11-05 16:47:52 -0600

    Add edit and delete buttons

--- src/js/dom-todo.js
+++ src/js/dom-todo.js
@@ -1,3 +1,5 @@
+import * as messager from './messager';
+
 export default class DomTodo {
   constructor() {
     this.displayId = -1;
@@ -7,7 +9,28 @@ export default class DomTodo {
     this.dueDate = document.createElement('time');
     this.priority = document.createElement('div');
     this.priority.className = 'priority';
-    this.element.append(this.title, this.description, this.dueDate, this.priority);
+    this.deleteBtn = document.createElement('button');
+    this.deleteBtn.className = 'delete-btn';
+    this.deleteBtn.textContent = 'Delete';
+    this.editBtn = document.createElement('button');
+    this.editBtn.className = 'edit-btn';
+    this.editBtn.textContent = 'Edit';
+    this.element.append(
+      this.title,
+      this.description,
+      this.dueDate,
+      this.priority,
+      this.editBtn,
+      this.deleteBtn,
+    );
+
+    this.deleteBtn.addEventListener('click', () => {
+      messager.postDeleteTodo(this.displayId);
+    });
+
+    this.editBtn.addEventListener('click', () => {
+      messager.postEditTodo(this.displayId);
+    });
   }
 
   appendTo(container) {
